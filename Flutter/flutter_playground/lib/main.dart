7cec55f97ea978439a32b6b3e62bb4a8da7e2dc6
isaias99i <isaiascarlos99i@gmail.com>
2022-03-27 21:10:23 -0300

    Listas Flutter

--- Flutter/flutter_playground/lib/main.dart
+++ Flutter/flutter_playground/lib/main.dart
@@ -29,28 +29,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final controller = TextEditingController();
+  final list = <String>[];
+
   @override
   Widget build(BuildContext context) {
-
-    final names = ['Isaias', 'Rickele', 'Larissa', 'Ysa', 'Carla', 'Ana'];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Curso Flutterando'),
       ),
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: ListView.builder(
-            itemCount: names.length,
-            itemBuilder: (context, index){
-            final name = names[index];
-              return ListTile(
-                title: Text(name),
-              );             
-            },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                    controller: controller,
+                    )
+                ),
+                IconButton(
+                  onPressed: () {
+                    final text = controller.text;
+                    setState(() {
+                      list.add(text);                     
+                    });
+                    controller.clear();
+                  }, 
+                  icon: const Icon(Icons.add)
+                  ),
+              ],
+            ),
           ),
-        ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                final item = list[index];
+                return ListTile(
+                  title: Text(item),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
