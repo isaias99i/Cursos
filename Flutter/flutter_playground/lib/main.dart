868b4f9bf7ad14a69945ac0e9894555ad53fb61f
isaias99i <isaiascarlos99i@gmail.com>
2022-04-13 20:31:12 -0300

    App Pedra, Papel Tesoura

--- Flutter/flutter_playground/lib/main.dart
+++ Flutter/flutter_playground/lib/main.dart
@@ -38,42 +38,42 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Curso Flutterando'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                    child: TextField(
-                    controller: controller,
-                    )
-                ),
-                IconButton(
-                  onPressed: () {
-                    final text = controller.text;
-                    setState(() {
-                      list.add(text);                     
-                    });
-                    controller.clear();
-                  }, 
-                  icon: const Icon(Icons.add)
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/imgs/background.png',
+                  fit: BoxFit.cover,
+                )),
+            Container(
+              color: Colors.black.withOpacity(0.7),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const FlutterLogo(
+                    size: 200,
+                    textColor: Colors.white,
+                    style: FlutterLogoStyle.horizontal,
                   ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: list.length,
-              itemBuilder: (context, index) {
-                final item = list[index];
-                return ListTile(
-                  title: Text(item),
-                );
-              },
-            ),
-          )
-        ],
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Entrar'),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
